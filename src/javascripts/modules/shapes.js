706d2fbf549c2b2d4a1d109d95901186ce9a01f3
Tom Walker <tom.w@niceandserious.com>
2018-11-15 16:28:00 +0000

    Update animation and add colours

--- src/javascripts/modules/shapes.js
+++ src/javascripts/modules/shapes.js
@@ -8,112 +8,106 @@ const masterTl = new TimelineMax({
   paused: true,
   repeat: -1,
   repeatDelay: 0.5
-})
-
-const triangleTl = new TimelineMax({
-
 });
 
+const triangleTl = new TimelineMax();
+
 triangleTl
-.timeScale(3)
+.timeScale(4)
 .to(triangle, 0.5, {
-  scaleX: 1.25,
-  scaleY: 0.75,
+  scaleX: 1.2,
+  scaleY: 0.8,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
+  yoyoEase: true,
+  ease: Power1.easeIn
 })
 .to(triangle, 0.5, {
-  y: -100,
+  y: -150,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
+  yoyoEase: true,
+  ease: Power1.easeOut
 })
 .to(triangle, 0.85, {
   rotation: 360,
-  transformOrigin: "50% 65%",
-  ease: Power0.easeNone
+  transformOrigin: "50% 66.66%",
+  ease: Power1.easeInOut
 }, "-=0.95")
 .to(triangle, 0.5, {
-  scaleX: 1.25,
-  scaleY: 0.75,
+  scaleX: 1.2,
+  scaleY: 0.8,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
-})
+  yoyoEase: true,
+  ease: Power1.easeOut
+});
 
-const squareTl = new TimelineMax({
-
-})
+const squareTl = new TimelineMax();
 
 squareTl
-.timeScale(3)
+.timeScale(4)
 .to(square, 0.5, {
-  scaleX: 1.25,
-  scaleY: 0.75,
+  scaleX: 1.2,
+  scaleY: 0.8,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
+  yoyoEase: true,
+  ease: Power1.easeIn
 })
 .to(square, 0.5, {
-  y: -100,
+  y: -150,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
+  yoyoEase: true,
+  ease: Power1.easeOut
 })
 .to(square, 0.85, {
   rotation: 360,
   transformOrigin: "50% 50%",
-  ease: Power0.easeNone
+  ease: Power1.easeInOut
 }, "-=0.95")
 .to(square, 0.5, {
-  scaleX: 1.25,
-  scaleY: 0.75,
+  scaleX: 1.2,
+  scaleY: 0.8,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
-})
+  yoyoEase: true,
+  ease: Power1.easeOut
+});
 
-const circleTl = new TimelineMax({
-
-})
+const circleTl = new TimelineMax();
 
 circleTl
-.timeScale(3)
+.timeScale(4)
 .to(circle, 0.5, {
-  scaleX: 1.25,
-  scaleY: 0.75,
+  scaleX: 1.2,
+  scaleY: 0.8,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
+  yoyoEase: true,
+  ease: Power1.easeIn
 })
 .to(circle, 0.5, {
-  y: -100,
+  y: -150,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
+  yoyoEase: true,
+  ease: Power1.easeOut
 })
 .to(circle, 0.5, {
-  scaleX: 1.25,
-  scaleY: 0.75,
+  scaleX: 1.2,
+  scaleY: 0.8,
   transformOrigin: "50% 100%",
   repeat: 1,
-  yoyo: true,
-  ease: Power0.easeNone
-})
+  yoyoEase: true,
+  ease: Power1.easeOut
+});
 
 const shapes = {
   init() {
-    masterTl.add([triangleTl, squareTl, circleTl], 0, "start", 0.2)
-    masterTl.play()
+    masterTl.add([triangleTl, squareTl, circleTl], 0, "start", 0.2);
+    masterTl.play();
   }
 }
 
